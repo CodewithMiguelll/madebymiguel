07ebc2af3dd3ee64d897709723053fdc0a1349a4
Miguel <113023936+CodewithMiguelll@users.noreply.github.com>
2025-12-07 18:49:03 +0100

    feat: enhance layout metadata and improve structure for better readability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,57 @@
 import type { Metadata } from "next";
-import Navigation from "@/components/navbar";
 import "./globals.css";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 import CustomCursor from "@/components/custom-cursor";
 import MatrixRain from "@/components/matrix-rain";
-import Footer from "@/components/footer";
-
 
 export const metadata: Metadata = {
-  title: "Chikaima .U | Portfolio Website",
-  description: "Chikaima's portfolio website",
+  title: {
+    default: "Chikaima Uwakwe | Portfolio Website",
+    template: "Chikaima Uwakwe | %s",
+  },
+  description:
+    "Portfolio website of Chikaima Miguel Uwakwe, a software developer specializing in building modern web applications, and product design.",
   icons: {
-    icon: "/assets/favicon.ico",
+    icon: [
+      { url: "/assets/favicon.ico" },
+      { url: "/assets/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/assets/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
     apple: "/assets/apple-touch-icon.png",
+  },
+  openGraph: {
+    title: "Chikaima Uwakwe | Portfolio Website",
+    description:
+      "Portfolio website of Chikaima Miguel Uwakwe, a software developer specializing in building modern web applications, and product design.",
+    url: "https://kaimaaa.vercel.app",
+    siteName: "Chikaima Uwakwe",
+    images: [
+      {
+        url: "/assets/chikaima-portfolio-og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Chikaima Uwakwe | Portfolio Website",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
   },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={`antialiased bg-[#141414] text-[#f5f5f5] cursor-none relative`}>
-        <MatrixRain/>
-        <Navigation />
-        <main suppressHydrationWarning>{children}</main>
+    <html lang="en" className="scroll-smooth">
+      <body className="bg-[#141414] cursor-none relative">
+        <Navbar />
+        <main>{children}</main>
         <Footer />
         <CustomCursor />
+        <MatrixRain />
       </body>
     </html>
   );
